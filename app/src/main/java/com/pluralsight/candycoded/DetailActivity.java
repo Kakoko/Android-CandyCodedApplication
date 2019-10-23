62cded647e007540ff009d39ed52d35585ac3005
Simuzeche Kaluwa <Skaluwa@nitel.me>
2019-10-23 14:46:56 +0200

    Added Share Activity Intent Fixed Typos of Typo Fixed

--- app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
@@ -76,13 +76,13 @@ public class DetailActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        createdShareIntent();
+        createShareIntent();
         return super.onOptionsItemSelected(item);
     }
 
 
 
-    private void createdShareIntent(){
+    private void createShareIntent(){
         Intent shareIntent = new Intent(Intent.ACTION_SEND);
         shareIntent.setType("text/plain");
 
